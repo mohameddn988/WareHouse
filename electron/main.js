8642e008fff61e68b650013b12e5095d87d8263c
mohameddn988 <msideneche@gmail.com>
2025-10-12 22:06:45 +0100

    Initial commit

---
+++ electron/main.js
@@ -0,0 +1,23 @@
+// electron/main.js
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // In dev mode, load Next.js; in prod, load local files
+  const startUrl = process.env.ELECTRON_START_URL || "http://localhost:3000";
+  win.loadURL(startUrl);
+}
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
